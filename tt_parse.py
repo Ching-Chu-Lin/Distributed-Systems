0f3b9b14b5c329a9ed5ef9f3af190f3ac33a2652
Ching-Chu-Lin <b06902111@ntu.edu.tw>
2024-03-15 13:18:06 -0700

    remove non alphanumeric

--- tt_parse.py
+++ tt_parse.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('./tiktok_app_reviews.csv',
                  usecols=['app_version', 'review_text'])
-df.dropna()
+df = df.dropna()
+df = df.review_text.str.replace('[^a-zA-Z 0-9]', '')
 
 df.to_csv('tiktok_parsed.csv', header=True, index=False)
