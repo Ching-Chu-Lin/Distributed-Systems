ee868a8e54043b5451a9dcf0541bee54d5e3d6a4
DoYeon Kim <52477309+vimpizz@users.noreply.github.com>
2024-03-15 14:51:30 -0700

    Update tt_parse.py

--- tt_parse.py
+++ tt_parse.py
@@ -3,6 +3,6 @@ import pandas as pd
 df = pd.read_csv('./tiktok_app_reviews.csv',
                  usecols=['app_version', 'review_text'])
 df = df.dropna()
-df = df.review_text.str.replace('[^a-zA-Z 0-9]', '')
+df['review_text'] = df['review_text'].str.replace('[^a-zA-Z 0-9]', '')
 
 df.to_csv('tiktok_parsed.csv', header=True, index=False)
